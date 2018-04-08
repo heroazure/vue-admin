999597ffa82423523b1898ab05e150af4a6dd3f1
肖黎军 <joylee@jollycorp.com>
2018-04-08 16:31:28 +0800

    '速度快看到'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import iView from 'iview'
+import {Breadcrumb, BreadcrumbItem, Button, RadioGroup, Radio, Row, Col, Input, Table, Page, Form, FormItem, Select, Option, Icon, Card} from 'iview'
 import router from './router'
 import App from './App.vue'
 import 'iview/dist/styles/iview.css'
@@ -8,7 +8,23 @@ import 'iview/dist/styles/iview.css'
 import LayoutMain from './components/layout-main'
 import ListSearch from './components/list-search'
 
-Vue.use(iView)
+// Vue.use(iView)
+Vue.component('Breadcrumb',Breadcrumb)
+Vue.component('Breadcrumb-item',BreadcrumbItem)
+Vue.component('Button',Button)
+Vue.component('RadioGroup',RadioGroup)
+Vue.component('Radio',Radio)
+Vue.component('Row',Row)
+Vue.component('Col',Col)
+Vue.component('Input',Input)
+Vue.component('Table',Table)
+Vue.component('Page',Page)
+Vue.component('Form',Form)
+Vue.component('FormItem',FormItem)
+Vue.component('Select',Select)
+Vue.component('Option',Option)
+Vue.component('Icon',Icon)
+Vue.component('Card',Card)
 // Vue.component(InputItem.name,InputItem)
 // Vue.component(Editor.name,Editor)
 Vue.component(LayoutMain.name,LayoutMain)
