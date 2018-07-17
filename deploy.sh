486929ba5fc5ccb513b7df7fe6fe5e52c26bec17
肖黎军 <joylee@jollycorp.com>
2018-07-17 15:47:55 +0800

    '修改deploy.sh文件'

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-LOG_FILE="log_deploy.log"
-
-date >> "$LOG_FILE"
-echo "Start deployment" >>"$LOG_FILE"
-echo "pulling source code..." >> "$LOG_FILE"
+echo "Start deployment"
+echo "pulling source code..."
 git reset --hard origin/develop
 git clean -f
 git pull
 git checkout develop
-echo "Finished." >>"$LOG_FILE"
-echo >> $LOG_FILE
+yarn run build
+echo "Finished."
