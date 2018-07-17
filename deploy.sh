5d99d59a9f2431d6abf1050d2b56bfcaa734f6d0
肖黎军 <joylee@jollycorp.com>
2018-07-17 16:42:48 +0800

    '添加pm2'

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 echo "Start deployment"
 echo "pulling source code..."
+
 git reset --hard origin/develop
 git clean -f
 git pull
 git checkout develop
+yarn
 yarn run build
+pm2 restart vue-admin
+
 echo "Finished."
