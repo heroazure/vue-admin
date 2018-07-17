62183edbca41a5f75d4a8f7769ef4d1502e05186
肖黎军 <joylee@jollycorp.com>
2018-07-17 11:49:56 +0800

    '修改deploy.js端口为3340'

--- deploy.js
+++ deploy.js
@@ -22,7 +22,7 @@ http.createServer(function (req, res) {
     res.statusCode = 404
     res.end('no such location')
   })
-}).listen(7777)
+}).listen(3340)
 
 handler.on('error', function (err) {
   console.error('Error:', err.message)
